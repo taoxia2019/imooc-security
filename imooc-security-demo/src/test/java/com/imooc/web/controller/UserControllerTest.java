183ec52dfb6f5364fe5c5647dd6caf8dc7cf16ba
taoxia <48041605+taoxia2019@users.noreply.github.com>
2019-06-21 22:56:44 +0800

    测试修改
    添加新方法

--- imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
+++ imooc-security-demo/src/test/java/com/imooc/web/controller/UserControllerTest.java
@@ -13,6 +13,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 /**
  * @ClassName UserControllerTest
  * @Description DOTO
@@ -46,5 +50,44 @@ public class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("tom"));
+    }
+
+    @Test
+    public void whenPostSuccess() throws Exception {
+
+        Date date=new Date();
+        System.out.println(date.getTime());
+        String content="{\"username\":\"tom\",\"password\":null,\"birthday\":"+date.getTime()+"}";
+        String result=mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(content))
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                        .andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenUpdateSuccess() throws Exception {
+
+        Date date=new Date(LocalDateTime.now().plusYears(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        System.out.println(date.getTime());
+        String content="{\"id\":\"1\",\"username\":\"tom\",\"password\":null,\"birthday\":"+date.getTime()+"}";
+        String result=mockMvc.perform(MockMvcRequestBuilders.put("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenDeleteSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
